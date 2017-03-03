fafbad99e9479003a5a0dba5836c0ce014cca3d0
Troels Liebe Bentsen <tlb@nversion.dk>
2017-03-03 13:13:15 +0100

    Added empty unit test

---
+++ WebApiJwtAuthorization.Tests/SimpleWebApiTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApiJwtAuthorization.Tests
+{
+
+    public class SimpleWebApiTests
+    {
+        public SimpleWebApiTests()
+        {
+            
+        }
+
+        [Fact]
+        public void SimpleGetTest()
+        {
+            
+        }
+    }
+}
